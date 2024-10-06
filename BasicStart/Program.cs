53514b4bbbbff42ca07caedb2555fe56c2afc8b9
Shashank Ushanagalla <128900949+UshanagallaShashank@users.noreply.github.com>
2024-10-06 15:31:24 +0530

    JQuery handling

--- BasicStart/Program.cs
+++ BasicStart/Program.cs
@@ -1,6 +1,23 @@
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.Run(  async(HttpContext context) =>
+{
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
+
+
+});
 
 app.Run();
