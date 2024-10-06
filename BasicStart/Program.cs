d96895327d348192ed65f33d867138b6832f656c
Shashank Ushanagalla <128900949+UshanagallaShashank@users.noreply.github.com>
2024-10-06 15:45:16 +0530

    Request Headers

--- BasicStart/Program.cs
+++ BasicStart/Program.cs
@@ -3,21 +3,39 @@ using System.IO;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
-app.Run(  async(HttpContext context) =>
+
+
+////-------JQuery Handling-----------------
+//app.Run(async (HttpContext context) =>
+//{
+//    context.Response.Headers["Content-type"] = "text/html";
+//    if (context.Request.Method == "GET")
+//    {
+//        if (context.Request.Query.ContainsKey("id"))
+//        {
+//            string id = context.Request.Query["id"];
+//            await context.Response.WriteAsync($"<p>{id}</p>");
+//        }
+//    }
+//}
+//);
+
+
+//-----------------------Request Headers-------------------------
+app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["Content-type"] = "text/html";
-    if (context.Request.Method == "GET")
-    {
-        if (context.Request.Query.ContainsKey("id"))
+    context.Response.Headers["Content-Type"] = "text/html";
+  
+        if (context.Request.Headers.ContainsKey("User-Agent"))
         {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<p>{id}</p>");
+            string userAgent = context.Request.Headers["User-Agent"];
+            await context.Response.WriteAsync($"<p>{userAgent}</p>");
         }
     }
 
+);
 
-});
 
 app.Run();
